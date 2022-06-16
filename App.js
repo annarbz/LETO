64063c15ad4898edeb56f15aba192e9817256a0f
annarbz <103678481+annarbz@users.noreply.github.com>
2022-06-16 16:30:34 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import {Button} from 'react-bootstrap';
+import Header from './Components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div >
+      <Header/>
+      
+    </div>
+    
+  );
+}
+
+export default App;
