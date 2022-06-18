2039e7edc762b6e3e2d66b07fa45ab54dd5c75ba
annarbz <103678481+annarbz@users.noreply.github.com>
2022-06-18 14:03:31 +0300

    Add files via upload

---
+++ src/Pages/Contacts.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import CarouselBox from '../Components/CarouselBox';
+
+export default class Contacts extends Component {
+    render() {
+        return (
+            <CarouselBox/>
+        );
+    }
+}
+
