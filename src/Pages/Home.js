679b7b6f8bffd426c7dd2ad1421b00ad8dfe9521
annarbz <103678481+annarbz@users.noreply.github.com>
2022-06-19 12:51:18 +0300

    Add files via upload

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
-import CarouselBox from '../Components/CarouselBox';
+import CarouselBox from '../Components/CarouselBox'
+
 
 export default class Home extends Component {
     render() {
         return (
             <div>
-                <CarouselBox/>
-                shbd
-                <h3>Ybgg</h3>
+                <CarouselBox />
+
 
             </div>
-            
+
         );
     }
 }
---
+++ src/Pages/Reviews.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export default class Reviews extends Component {
+    render() {
+        return (
+            <h1>about</h1>
+        )
+    }
+}
+
