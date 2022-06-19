fd483396145de70ed8d04ab211d7f39917589c4e
annarbz <103678481+annarbz@users.noreply.github.com>
2022-06-19 12:51:00 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-import Header from './Components/Header';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './Components/Header'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import CarouselBox from './Components/CarouselBox'
 
 
 function App() {
   return (
-    <div >
-      <Header/>
-      
+    <div>
+      <Header />
+      <CarouselBox />
+      <a class="el-content uk-button uk-button-default uk-button-large" href="ьь">
+
+        ЗАБРОНИРОВАТЬ
+
+      </a>
+      <p>
+        О НАС
+      </p>
+      <h2 class="uk-heading-medium uk-text-left uk-scrollspy-inview " uk-scrollspy-class="" >     О НАС    </h2>
+
+
+
+
+
     </div>
-    
-  );
+  )
 }
 
-export default App;
+export default App
