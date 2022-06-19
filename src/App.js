26e35cca82ed7c563dfa6b7d8ff339ea204df257
annarbz <103678481+annarbz@users.noreply.github.com>
2022-06-19 15:27:07 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,31 +1,20 @@
 import React from 'react'
 import './App.css'
 
-import Header from './Components/Header'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import CarouselBox from './Components/CarouselBox'
+import { BrowserRouter as Router } from 'react-router-dom'
 
+import Header from './Components/Header'
+import { AppRoutes } from './AppRoutes'
 
 function App() {
   return (
-    <div>
-      <Header />
-      <CarouselBox />
-      <a class="el-content uk-button uk-button-default uk-button-large" href="ьь">
-
-        ЗАБРОНИРОВАТЬ
-
-      </a>
-      <p>
-        О НАС
-      </p>
-      <h2 class="uk-heading-medium uk-text-left uk-scrollspy-inview " uk-scrollspy-class="" >     О НАС    </h2>
-
-
-
-
-
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <AppRoutes />
+      </div>
+    </Router>
   )
 }
 
